0766be97137c738c5eb92810a442a686b283a015
Rajeev Chauhan <92995223+rajchauhan9@users.noreply.github.com>
2023-02-20 23:40:53 +0530

    Update git.tf

--- git.tf
+++ git.tf
@@ -1,15 +1,3 @@
-
-Pre-Requisite
-Make sure you create your GitHub account.
-Create Personal Access Token
-GitHub Provider Terraform:
-https://registry.terraform.io/providers/integrations/github/latest/docs
-
-Code Used:
-
-
-
-
 terraform {
   required_providers {
     github = {
